12f0eec79ea73f8edaef28fe186d2c48482cceee
Erik Thiem <erikthiem@gmail.com>
2013-03-12 19:25:30 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+	end
+
   def new
+		@user = User.new
   end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			# Welcome message
+			first_name_capitalized = @user.name.split(" ")[0].capitalize
+			message = "Welcome to the sample app, " + first_name_capitalized + "."	
+			message += " Thank you for making an account."
+
+			flash[:success] = message
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 end
